0a9e6a77317f1aa5a2fd2c4582e02207067922c5
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2019-08-15 15:21:20 +0500

    more in TryRectangles.java

--- TryRectangles.java
+++ TryRectangles.java
@@ -4,5 +4,8 @@ class TryRectangles {
   public static void main(String[] args) {
     Rectangle r1 = new Rectangle(1, 1, 2, 0);
     System.out.println(r1);
+
+    Rectangle r2 = new Rectangle(-1, 10, 3, 1);
+    System.out.println(r2.getEnclosingRect(r1));
   }
 }
